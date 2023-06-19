1231c14b7aafa442fd54629cd14b327d68253c67
Valerii <valeriy.stronskiy@gmail.com>
2023-06-19 11:51:48 +0300

    Created the Storage database, added the connection string to the Config.
    Filled the Storage database with tables: Products, Providers, ProductsPictures.
    
    In addition, there is a file with a script for creating tables.

---
+++ Creating tables for StorageDB.sql
@@ -0,0 +1,27 @@
+﻿create table Providers
+(
+	Id int not null primary key,
+	Name varchar(50) not null,
+	Phone varchar(13) not null
+)
+
+create table Products
+(
+	Id int not null primary key,
+	Name varchar(max) not null,
+	Quantity int not null,
+	CostPrice money not null,
+	DeliveryDate date not null,
+	ProviderId int not null,
+
+	foreign key (ProviderId) references Providers (Id) 
+)
+
+create table ProductsPictures
+(
+	Id int not null primary key,
+	Picture varbinary(max) not null,
+	ProductId int not null,
+
+	foreign key (ProductId) references Products (Id)
+)
